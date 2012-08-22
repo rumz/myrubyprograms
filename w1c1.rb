038cb7190ad60d14f5780eac051555b28917c5b5
Rommel Asibal <rumzster@gmail.com>
2012-08-22 10:33:02 -0400

    Refactored w1c1.rb and Added Additional Functionality

--- w1c1.rb
+++ w1c1.rb
@@ -1,41 +1,45 @@
 =begin
 
 Using what we have learned so far: I wonder if anyone could have a crack at working out the age in years and months form. For example “I’m 32 years and 6 months old.”
-Use the following values for age_in_seconds:
-
-979000000
-2158493738
-246144023
-1270166272
-1025600095
-
- Seconds
- 60       = 1 min  = 1 sec x 60
- 3600     = 1 hr   = 60 min x 60 secs
- 86400    = 1 day  = 3600 secs x 24  
- 31536000 = 1 year = 365 days x 86400 secs
 
 =end
 
-SECONDS_IN_YEAR = 31536000
-SECONDS_IN_MONTH = 86400 * 30
+second = 1
+seconds_in_minute = second * 60
+seconds_in_hour = 60 * seconds_in_minute
+seconds_in_day = 24 * seconds_in_hour
+@seconds_in_year = 365 * seconds_in_day
+@seconds_in_month = 30 * seconds_in_day
 
 def convert_secs_years(age_in_seconds)
-  format("%2d", (age_in_seconds / SECONDS_IN_YEAR))
+  age_in_seconds / @seconds_in_year
 end
 
 def convert_secs_months(age_in_seconds)
-  format("%2d", ((age_in_seconds % SECONDS_IN_YEAR)/SECONDS_IN_MONTH))
+  (age_in_seconds % @seconds_in_year) / @seconds_in_month
 end
 
 def show_age(age_in_seconds)
-  puts "I'm #{convert_secs_years(age_in_seconds)} years\
- and #{convert_secs_months(age_in_seconds)} months old."
+  puts "I'm #{format("%2d", convert_secs_years(age_in_seconds))} years\
+ and #{format("%2d", convert_secs_months(age_in_seconds))} months old."
 end
 
+
+ages = []
+ages.push(convert_secs_years(979000000))
+ages.push(convert_secs_years(2158493738))
+
+total_years = 0
+
+ages.each { |age| total_years += age } 
+
+puts "Total years: #{total_years}"
+puts "Average number of years: #{total_years.to_f / ages.size}"
+
+
 # Test Ages 
-show_age(979000000)
-show_age(2158493738)
-show_age(246144023)
-show_age(1270166272)
-show_age(1025600095)
+# show_age(979000000)
+# show_age(2158493738)
+# show_age(246144023)
+# show_age(1270166272)
+# show_age(1025600095)
