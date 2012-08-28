d959ac47b6dab5c0a2c1b6626205d42ef24439be
Rommel Asibal <rumzster@gmail.com>
2012-08-28 10:35:44 +0800

    Refactored exer 3 & 6

--- exercise_003.rb
+++ exercise_003.rb
@@ -1,29 +1,24 @@
-# Author: Rommel Asibal
-# Exercise 3
+=begin
+	Author: Rommel Asibal
+	Exercise 3
 
-# Write a Ruby program that displays how old I am, if I am 979000000 seconds old. Display the result as a floating point (decimal) number to two decimal places (for example, 17.23).
-# Note: To format the output to say 2 decimal places, we can use the Kernel's format method. For example, if x = 45.5678 then format("%.2f", x) will return the string 45.57
+	Write a Ruby program that displays how old I am, if I am 979000000 seconds old. Display the result as a floating point (decimal) number to two decimal places (for example, 17.23).
+	Note: To format the output to say 2 decimal places, we can use the Kernel's format method. For example, if x = 45.5678 then format("%.2f", x) will return the string 45.57
 
-# This program takes seconds and converts it to years
-
-  # 60 = 1 min
-  # 3600 = 1 hr
-  # 86400 = 1 day
-  # 31536000 = 1 year
+	This program takes seconds and converts it to years
+=end
 
 def ConvertSecondsToYears( age_in_seconds )
-  age_in_seconds.to_f / 31536000.0
+  age_in_seconds.to_f / (60 * 60 * 24 * 365)
 end
 
-age = ConvertSecondsToYears(979000000)
-puts age.class
-age = format("%.2f", age)
-puts age
-puts age.class
+#age = ConvertSecondsToYears(seconds)
+#age = format("%.2f", age)
+#puts age
 
 puts "Give me a number in seconds and I will return the number of years it represents. "
 STDOUT.flush
 seconds = gets.chomp
-years = ConvertSecondsToYears(seconds)
+years = format("%.2f",ConvertSecondsToYears(seconds))
 puts "#{seconds} seconds equals #{years} years"
 
--- exercise_006.rb
+++ exercise_006.rb
@@ -2,14 +2,12 @@
 # To format the output to say 2 decimal places, we can use the Kernel's format method. For example, if x = 45.5678 then format("%.2f", x) will return the string 45.57. Another way is to use the round function as follows: 
 # puts (x*100).round/100.0
 
-def convert(celsius=0.0)
-  (celsius - 32) / 1.8
-
+def convert(temp_f=0.0)
+  (temp_f - 32) / 1.8
 end
 
 puts "Temperature in Farenheit: "
 STDOUT.flush
-farenheit = gets.chomp
-celsius = convert(farenheit.to_f)
-celsius = format("%.2f", celsius)
-puts "#{farenheit} degrees farenheit equals #{celsius} degrees celsius"
+temp_f = gets.chomp
+temp_c = format("%.2f", convert(temp_f.to_f))
+puts "#{temp_f} degrees farenheit equals #{temp_c} degrees celsius"
