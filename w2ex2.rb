cf9882938922c2a15ba0bcc1c8277f61c68566b0
Rommel Asibal <rumzster@gmail.com>
2012-09-02 05:13:08 +0800

    Added explanation for how the programs work
    The explanations are in the comments following the actual code.

--- w2ex2.rb
+++ w2ex2.rb
@@ -1,6 +1,6 @@
 #Exercise2. Run the following two programs and try and understand the difference in the outputs of the two programs. The program:
 
-=begin
+
 def mtdarry
  10.times do |num|
    puts num
@@ -8,10 +8,14 @@ def mtdarry
 end
 
 mtdarry
-=end
 
-#and the program:
 
+# This is calling the method mtdarry so it will output 0-9
+
+
+#and then run program:
+
+=begin
 def mtdarry
  10.times do |num|
    puts num
@@ -19,3 +23,8 @@ def mtdarry
 end
 
 puts mtdarry
+=end
+
+# This calls the method mtdarry in puts so first it outputs 0-9 
+# and then mtdarry returns 10 which puts then outputs to the screen as well
+# OT: I really like the implementation of NUMBER.times its really intuitive
