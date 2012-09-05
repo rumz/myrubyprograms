72a0b190f7bfba2c01b57df8a68d890ced3dd95c
Rommel Asibal <rumzster@gmail.com>
2012-09-05 14:33:16 +0800

    added w3ex 10-16 problem definition only

---
+++ w3ex11.rb
@@ -0,0 +1,4 @@
+=begin
+w3ex11.rb
+Exercise11. Write a Ruby program that, when given an array: collection = [12, 23, 456, 123, 4579] prints each number, and tells you whether it is odd or even.
+=end
