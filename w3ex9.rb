5c9c82f2ce69e726beee6ad309f2df2387bbcbce
Rommel Asibal <rumzster@gmail.com>
2012-09-08 17:31:30 +0800

    Added w3ex9.rb

--- w3ex9.rb
+++ w3ex9.rb
@@ -2,3 +2,7 @@
 w3ex9.rb
 Exercise9. Given a string, write a program to reverse the word order (rather than character order). The String and Array classes provide methods which will be very helpful for solving this exercise.
 =end
+
+string = "This is a sample string."
+new_string = string.split(' ').reverse.join(' ')
+puts new_string
