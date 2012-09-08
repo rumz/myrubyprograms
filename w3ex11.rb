ce6d11b41a1cfb79b9308805d0fb7c09e3c137c3
Rommel Asibal <rumzster@gmail.com>
2012-09-08 17:26:18 +0800

    Added w3ex11.rb

--- w3ex11.rb
+++ w3ex11.rb
@@ -1,4 +1,13 @@
+# w3ex11.rb
 =begin
-w3ex11.rb
 Exercise11. Write a Ruby program that, when given an array: collection = [12, 23, 456, 123, 4579] prints each number, and tells you whether it is odd or even.
 =end
+
+
+def parity(number)
+  (number % 2==0) ? 'Even' : 'Odd'
+end
+
+
+collection = [12, 23, 456, 123, 4579]
+collection.each { |number| puts "#{number} is #{parity(number)}" }
