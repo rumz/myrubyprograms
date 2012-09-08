84a0233622864a83d4e3b51bbd2f1dbffe23d1fb
Rommel Asibal <rumzster@gmail.com>
2012-09-08 17:49:06 +0800

    Added w3ex14.rb

--- w3ex14.rb
+++ w3ex14.rb
@@ -6,9 +6,22 @@ Select all answers which return true.
 h = { "Ruby" => "Matz", "Perl" => "Larry", "Python" => "Guido" }
 Answers:
 1. h.member?("Matz")
-2. h.member?("Python")
+2. h.member?("Python")  
 3. h.include?("Guido")
 4. h.include?("Ruby")
 5. h.has_value?("Larry")
 6. h.exists?("Perl")
 =end
+
+
+h = { "Ruby" => "Matz", "Perl" => "Larry", "Python" => "Guido" }
+
+puts h.member?("Matz")    
+puts h.member?("Python")
+puts h.include?("Guido")
+puts h.include?("Ruby")
+puts h.has_value?("Larry")
+puts h.exists?("Perl")
+
+# True  : 2, 4, 5
+# There is no h.exists? method
