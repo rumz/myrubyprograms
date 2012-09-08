5c738d492990f95e6bfe844f17c9621a16ebc8e6
Rommel Asibal <rumzster@gmail.com>
2012-09-08 17:32:37 +0800

    Refactored w3ex8.rb with Victor's help

--- w3ex8.rb
+++ w3ex8.rb
@@ -1,34 +1,22 @@
 =begin
+doctest: fizzbuzz works
+>> fizz_buzz(0)
+=> "FizzBuzz"
+
 w3ex8.rb
 Exercise8. Write a Ruby program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz". Discuss this in the FizzBuzz Forum.
 =end
 
 
-def fizz_buzz(num) 
+def fizzbuzz(num) 
   output = ''
-  if (num % 3==0)
-    output << 'Fizz'
-  end
-  if (num % 5==0)
-    output << 'Buzz'
-  end
-  if output == ''
-    output << num.to_s
-  end
-  return output
+  output << 'Fizz' if (num % 3==0)
+  output << 'Buzz' if (num % 5==0)
+  output.empty? and num or output
 end
 
-(1..100).each do |num|
-  puts fizz_buzz(num)
+if __FILE__ == $0
+  (1..15).each do |num|
+    puts fizzbuzz(num)
+  end
 end
-
-
-# def fizz_buzz(num) 
-#   output = case
-#     when ((num % 3==0) and (num % 5==0)) then "FizzBuzz"
-#     when (num % 5==0) then "Buzz"
-#     when (num % 3==0) then "Fizz"
-#     else num
-#     end
-#   return output
-# end
