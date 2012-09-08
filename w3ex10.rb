0191b2e6012621baffb0bade42d91d986173b843
Rommel Asibal <rumzster@gmail.com>
2012-09-08 13:34:35 +0800

    Added w3ex10.rb

--- w3ex10.rb
+++ w3ex10.rb
@@ -2,3 +2,9 @@
 w3ex10.rb
 Exercise10. Write a Ruby program that, when given an array: collection = [1, 2, 3, 4, 5] calculates the sum of its elements.
 =end
+
+numbers = [1, 2, 3, 4, 5]
+
+sum = 0
+numbers.each {|number| sum += number }
+puts sum
