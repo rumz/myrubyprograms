5655ae48241e8dbba3ea88350b0a4654b714d889
Rommel Asibal <rumzster@gmail.com>
2012-09-27 06:56:46 +0800

    difference.rb with changes from hangout with vgoff

--- difference.rb
+++ difference.rb
@@ -1,10 +1,11 @@
 # Second  script (diff)
 
-file1 = 'old-inventory.txt'
-file2 = 'new-inventory.txt'
-
-file1_contents = []
-file2_contents = []
+file1            = 'old-inventory.txt'
+file2            = 'new-inventory.txt'
+file1_contents   = []
+file2_contents   = []
+added_items      = []
+subtracted_items = []
 
 File.open(file1, "r") do |io|
   while line = io.gets
@@ -18,15 +19,12 @@ File.open(file2, "r") do |io|
   end  
 end if File::file?(file2) 
 
-added_items = []
-subtracted_items = []
-
 file1_contents.each { |item| subtracted_items.push(item) unless file2_contents.include?(item) }
 file2_contents.each { |item| added_items.push(item) unless file1_contents.include?(item) }
 
-puts "Files deleted from the original list"
-subtracted_items.each { |item| print "#{item.chomp}\n" }
+puts "Comparing #{file1} and #{file2} to see changes:"
+puts "Files deleted from the original directory listing"
+subtracted_items.each { |item| puts "#{item.chomp}" }
 puts 
-puts "Files added to the original list"
-added_items.each { |item| print "#{item.chomp}\n" }
-
+puts "Files added to the original directory listing"
+added_items.each { |item| puts "#{item.chomp}" }
