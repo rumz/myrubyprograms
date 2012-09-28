6e197db352f37c50f20e20d44ad14810b7dbc612
Rommel Asibal <rumzster@gmail.com>
2012-09-28 13:22:12 +0800

    w5ex3 refactor out no_of_hits

--- week5/gameboard.rb
+++ week5/gameboard.rb
@@ -1,26 +1,17 @@
 class GameBoard
   attr_accessor :locations
-
-  def initialize
-    @no_hits = 0
-  end
-
+  
   def set_locations_cells(locations)
     @locations = locations
   end
-
   def check_yourself(user_guess)
+    puts locations.size
     if locations.include?(user_guess)  
-      locations.delete(user_guess)
-      @no_hits += 1
-      if @no_hits == 3 
-        p 'End'
-        'kill'
-      else 
-        p 'Hit'
-    end
+        locations.delete(user_guess)
+        puts 'Hit'
     else
-      p 'Miss'  
-    end 
+        puts 'Miss'  
+    end       
+    'kill' if locations.size == 0
   end
 end
