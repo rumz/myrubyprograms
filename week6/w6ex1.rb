11ce521aaafdde06c4e468fcd98c41c219864f61
Rommel Asibal <rumzster@gmail.com>
2012-10-01 08:25:49 +0800

    w6ex1 balance

---
+++ week6/w6ex1.rb
@@ -0,0 +1,10 @@
+# Exercise1. Write a class called Person that has balance as an instance variable and make it readable via an accessor.
+
+class Person
+  attr_reader :balance
+
+  def initialize(balance)
+    @balance = balance
+  end 
+
+end
